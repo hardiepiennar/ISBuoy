7862d734108bd7a6d055375eb1dcb33661c49a36
Hardie Pienaar <hardiepienaar@gmail.com>
2014-08-05 22:49:24 +0200

    Added menu item sliders
    Added menu item slider.
    -added contrast slider
    -finished main menu prelim layout

--- ISBController/ISBController.ino
+++ ISBController/ISBController.ino
@@ -36,6 +36,8 @@ short menuState;
 short innerMenuState;
 short noOfMenuItems;
 
+/*Settings*/
+int contrast = 57;
 
 void setup()   {
   Serial.begin(9600);
@@ -90,6 +92,7 @@ void controlUpdate()
         break;
       
       case MENU_MAIN:
+      case MENU_SETTINGS:
         innerMenuState += encoderValue;
         /*Keep cursor from selecting invalid option*/
         if(innerMenuState < -1)
@@ -97,6 +100,12 @@ void controlUpdate()
         if(innerMenuState >= noOfMenuItems)
           innerMenuState = noOfMenuItems - 1;
       break;
+      
+      /*Change contrast value and set the new display contrast*/
+      case MENU_SETTINGS_CONTRAST:
+        contrast += encoderValue;
+        display.setContrast(contrast);
+        break;
     } 
   }
   
@@ -125,8 +134,39 @@ void controlUpdate()
              innerMenuState = MENU_BACK;
              noOfMenuItems = 0;
              break; 
+             
+           case MENU_SETTINGS_PLACEMENT:
+             menuState = MENU_SETTINGS;
+             innerMenuState = MENU_BACK;
+             noOfMenuItems = 0;
+             break; 
         }
       break;
+      
+      case MENU_SETTINGS:
+        /*Check which menu option is selected*/
+        switch(innerMenuState)
+        {
+           case MENU_BACK:
+             menuState = MENU_MAIN;
+             innerMenuState = MENU_BACK;
+             noOfMenuItems = 0;
+             break; 
+           
+           case MENU_CONTRAST_PLACEMENT:
+             menuState = MENU_SETTINGS_CONTRAST;
+             innerMenuState = MENU_BACK;
+             noOfMenuItems = 0;
+             break; 
+        }
+      break;
+      
+      case MENU_SETTINGS_CONTRAST:
+        menuState = MENU_SETTINGS;
+        innerMenuState = MENU_CONTRAST_PLACEMENT;
+        noOfMenuItems = 0;
+        break;
+      
     } 
   }
 }
@@ -150,12 +190,29 @@ void screenUpdate()
       drawHeader("MENU");
       /*Draw menu items*/
       noOfMenuItems = 0;
-      drawMenuItem("SET WIDTH",0);
-      drawMenuItem("SET LENGTH",1);
-      drawMenuItem("BUOY DIAG",2);
+      drawMenuItem("SESSION",MENU_SESSION_PLACEMENT);
+      drawMenuItem("COURSE",MENU_COURSE_PLACEMENT);
+      drawMenuItem("BUOYS",MENU_BOUYS_PLACEMENT);      
+      drawMenuItem("SETTINGS",MENU_SETTINGS_PLACEMENT);
       /*Draw selected cursor*/
       drawCursor();
     break;
+
+    case MENU_SETTINGS:
+    case MENU_SETTINGS_CONTRAST:
+      /*Draw header*/
+      drawHeader("SETTINGS");
+      /*Draw menu items*/
+      noOfMenuItems = 0;
+      if(menuState == MENU_SETTINGS_CONTRAST)
+        drawMenuSliderItem("CONTRAST",MENU_CONTRAST_PLACEMENT,contrast,0,100,1);
+      else
+        drawMenuSliderItem("CONTRAST",MENU_CONTRAST_PLACEMENT,contrast,0,100,0);
+      /*Draw selected cursor*/
+      drawCursor();
+      break;
+      
+     
   } 
   
   /*Redraw Screen*/
@@ -177,21 +234,50 @@ void drawCursor()
   }
   else
   {
-     display.fillRect(0,index*(LINE_HEIGHT +1) + HEADER_HEIGHT,CURSOR_WIDTH,LINE_HEIGHT,BLACK);
+     display.fillRect(0,index*(LINE_HEIGHT +LINE_SPACER) + HEADER_HEIGHT+LINE_SPACER,CURSOR_WIDTH,LINE_HEIGHT,BLACK);
   }
 }
 
+/*Draws a menu item with a slider function, the menu item displays the current value when inactive.
+When active, the string is replaced by the slider which is then drawn as specified with min max and
+its current value*/
+void drawMenuSliderItem(char* string,short index,int value,int minValue,int maxValue,short active)
+{
+    int barLength = 60;
+    char buffer[3];
+    display.setTextColor(BLACK);
+    display.setTextSize(1);
+    
+    if(active == 0)
+    {
+      //Draw name of slider
+      display.setCursor(CURSOR_WIDTH+1,index*(LINE_HEIGHT + LINE_SPACER)+HEADER_HEIGHT+ LINE_SPACER);
+      display.println(string);           
+    }
+    else
+    {
+      display.drawRect(0,index*(LINE_HEIGHT + LINE_SPACER)+HEADER_HEIGHT+ LINE_SPACER,barLength,LINE_HEIGHT,BLACK);
+      display.fillRect(0,index*(LINE_HEIGHT + LINE_SPACER)+HEADER_HEIGHT+ LINE_SPACER,(int)((barLength*value/(maxValue-minValue))),LINE_HEIGHT,BLACK);
+    }
+    
+    //Draw slider value
+    display.setCursor(display.width() - 20,index*(LINE_HEIGHT + LINE_SPACER)+HEADER_HEIGHT+ LINE_SPACER);
+    display.println(itoa(value,buffer,10));
+    
+    /*Increase the number of menu items available*/
+    noOfMenuItems += 1;
+}
 /*Draws a menu item under the header location with a 
 given index. This allows menu items to be easily swapped
 in the configuration. Menu items should have names 10 
 characters or less, its assumed that the 0 state is the
 root state, therefore it will not have any back
 button*/
-void drawMenuItem(char* string,int index)
+void drawMenuItem(char* string,short index)
 {
     display.setTextColor(BLACK);
     display.setTextSize(1);
-    display.setCursor(CURSOR_WIDTH+1,index*(LINE_HEIGHT +1)+HEADER_HEIGHT);
+    display.setCursor(CURSOR_WIDTH+1,index*(LINE_HEIGHT + LINE_SPACER)+HEADER_HEIGHT+ LINE_SPACER);
     display.println(string);
     
     /*Increase the number of menu items available*/
@@ -205,7 +291,8 @@ void drawHeader(char* string)
     display.setTextSize(1);
     display.setCursor(0,0);
     display.println(string);
-    display.drawLine(0,(HEADER_HEIGHT-2),display.width(),(HEADER_HEIGHT)-2,BLACK);
+    display.drawLine(0,(HEADER_HEIGHT- LINE_SPACER),display.width(),(HEADER_HEIGHT)- LINE_SPACER,BLACK);
+    display.drawLine(0,(HEADER_HEIGHT- LINE_SPACER)-1,display.width(),(HEADER_HEIGHT)- LINE_SPACER-1,BLACK);
     
     if(menuState != MENU_STATUS)
     {
@@ -245,7 +332,7 @@ void initScreen()
   display.begin();
   
   /*Set the contrast of the display*/
-  display.setContrast(57);
+  display.setContrast(contrast);
 }
 
 /*Interrupt on encoder events*/
--- ISBController/configuration.h
+++ ISBController/configuration.h
@@ -3,8 +3,9 @@
 
 /*Menu layout definitions*/
 #define LINE_HEIGHT 7
-#define HEADER_HEIGHT 10
-#define BACK_BUTTON_START 47
+#define LINE_SPACER 2
+#define HEADER_HEIGHT 11
+#define BACK_BUTTON_START 51
 #define BACK_BUTTON_CURSOR_WIDTH 8
 #define CURSOR_WIDTH 3
 
@@ -12,6 +13,23 @@
 #define MENU_STATUS 0
 #define MENU_MAIN 1
 #define MENU_BACK -1
+//Main menu state
+#define MENU_SESSION 2
+#define MENU_COURSE 3
+#define MENU_BOUYS 4
+#define MENU_SETTINGS 5
+//Settings state
+#define MENU_SETTINGS_CONTRAST 6
+
+
+/*Define menu orders*/
+/*Main Menu*/
+#define MENU_SESSION_PLACEMENT 0
+#define MENU_COURSE_PLACEMENT 1
+#define MENU_BOUYS_PLACEMENT 2
+#define MENU_SETTINGS_PLACEMENT 3
+/*Settings*/
+#define MENU_CONTRAST_PLACEMENT 0
 
 /*Define encoder settings*/
 #define ENCODER_STEPS_PER_NOTCH 4
