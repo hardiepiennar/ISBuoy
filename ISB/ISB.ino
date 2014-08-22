396eacb83ab4c09b8a888ba57317e435f75ba1fd
Hardie Pienaar <hardiepienaar@gmail.com>
2014-08-22 09:28:32 +0200

    Added Adafruit GPS Library
    GPS library example implemented.
    Printing parsed details to seial

--- ISB/ISB.ino
+++ ISB/ISB.ino
@@ -9,17 +9,28 @@
 #include <nRF24L01.h>
 #include <RF24Network.h>
 #include <RF24.h>
-
+#include <Adafruit_GPS.h>
+#include <Adafruit_GPS.h>
 #include <Wire.h> //I2C Arduino Library
 
 /*Setup software UART port*/
 #define GPSRATE 9600
 #define BUFFSIZ 90 // plenty big
-
 char buffidx;
 char buffer[BUFFSIZ];
-
 SoftwareSerial mySerial =  SoftwareSerial(GPS_TX, GPS_RX);
+Adafruit_GPS GPS(&mySerial);
+
+//GPS Settings
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences. 
+#define GPSECHO  false
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+uint32_t timer = millis();
+
 
 #define address 0x1E //0011110b, I2C 7bit address of HMC5883
 
@@ -67,13 +78,13 @@ int declinationAngle  = 0;
 
 
 void setup() {  
-  /*Test GPS*/
-  Serial.begin(GPSRATE);
-  mySerial.begin(GPSRATE);
+  /*Initialise GPS*/
+  initGPS();
   
+  /*Initialise Radio*/
   initRF();
   
-  //Initialize Serial and I2C communications
+  //Initialise Serial and I2C communications
   Serial.begin(9600);
   Wire.begin();
   
@@ -84,6 +95,21 @@ void setup() {
   Wire.endTransmission();
 }
 
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
 void loop() 
 {
   /*Update the mesh network*/
@@ -91,9 +117,10 @@ void loop()
   checkIfPacketsAreAvailable();  
   
   /*Read sensors*/
+  readGPS();
   
   /*Navigation*/
-  
+
   
   /*Test peripherals*/
   //char buffer[10];
@@ -294,10 +321,102 @@ int readCompass()
 void initRF()
 {    
     radio.begin();
-    //radio.setRetries(15,15);
     network.begin(/*channel*/ 90, /*node address*/ BUOY_G1_ADDRESS);
-    //radio.setPayloadSize(9);
-    /*radio.openWritingPipe(controlRXPipe);
-    radio.openReadingPipe(1,controlTXPipe);
-    radio.startListening();*/
 }
+
+/*Initialise GPS with all of its settings, from adafruit example*/
+void initGPS()
+{
+  GPS.begin(9600); 
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+
+  delay(1000);
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
+}
+
+void readGPS()
+{
+   // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) { 
+    timer = millis(); // reset the timer
+    
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", "); 
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    }
+  }
+}  
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
